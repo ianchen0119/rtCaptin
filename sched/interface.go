ad71c4378f6bf35510d884384be9005938dc1aec
Ian Chen <ychen.desl@gmail.com>
2023-04-09 16:21:53 +0800

    update interface definitions

--- sched/interface.go
+++ sched/interface.go
@@ -1,6 +1,24 @@
 package sched
 
-type jobOps interface {
-	DefineNewJob()
-	CreateNewJob()
+import "context"
+
+type captinOps interface {
+	NewScheduler(string, int) (*Scheduler, error)
 }
+
+type schedulerOps interface {
+	DefineNewJob(string, bool, int, bool,
+		func(JobContext, interface{})) error
+	CreateNewJob(string, interface{},
+		[]*Resource) (<-chan interface{}, error)
+	Start(context.Context)
+}
+
+type jobOps interface {
+	NewJobContext() JobContext
+}
+
+// compile check
+var _ captinOps = &Captin{}
+var _ schedulerOps = &Scheduler{}
+var _ jobOps = &Job{}
